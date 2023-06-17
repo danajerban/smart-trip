312c23dfb4597d1a09ce715c5594243c0713366b
angusmellor <theangusmellor@gmail.com>
2023-06-17 12:03:12 +0100

    dependacy clean

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,7 @@
-import React from 'react';
 import styles from "./homeStyles.module.css"
-import { toast } from 'react-toastify'
 
 const Home = () => {
-  // toast.info('Explore on your own or AI Chat?', {
-  //   position: "top-center",
-  //   autoClose: 4000,
-  //   hideProgressBar: true,
-  //   closeOnClick: true,
-  //   pauseOnHover: true,
-  //   draggable: true,
-  //   progress: undefined,
-  //   theme: "dark",
-  //   });
+
   return (
 
       <main className={`${styles.flexHome} ${styles.bodyHome}`}>
