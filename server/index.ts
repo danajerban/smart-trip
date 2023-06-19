c9aefedd55d380597a10dd7d057a99b3d272d159
Luis Carrasco <luis@8ventures.io>
2023-06-19 18:54:01 +0200

    tests done in backend

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,5 @@
-require('dotenv').config({ path: '../.env' });
-import express from 'express';
-import router from './router';
-import cors from 'cors';
-
-const HOST = process.env.HOST_NAME || 'localhost';
-const PORT = Number(process.env.PORT_NUMBER) || 3000;
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(router);
-
-router.get('*', (_req: express.Request, res: express.Response) => {
-  res.status(404).send('Route not found');
-});
+import app from './app';
+const { HOST, PORT } = require('./config.ts');
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running at http://${HOST}:${PORT}`);
--- server/tests/index.test.ts
+++ server/tests/index.test.ts
@@ -1,7 +1,45 @@
-function sum(a: number, b: number) {
-  return a + b;
-}
+const request = require('supertest');
+import app from '../app';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('Sanity test', () => {
+  test('1 should equal 1', () => {
+    expect(1).toBe(1);
+  });
+});
+
+describe('Not found endpoint', () => {
+  test('should return "Route not found" for any URL other than /openai/chat', async () => {
+    const urlsToTest = ['/', '/openai', '/openai/chatroom', '/api', '/users'];
+
+    for (const url of urlsToTest) {
+      const res = await request(app).get(url);
+      expect(res.status).toEqual(404);
+      expect(res.text).toEqual('Route not found');
+    }
+  });
+});
+
+describe('OpenAI chat endpoint', () => {
+  test('should return 200 if request is valid', async () => {
+    await request(app)
+      .post('/openai/chat')
+      .set('Content-Type', 'application/json')
+      .send({ message: 'are you going to take over the world?' })
+      .expect(200);
+  });
+
+  test('should return 400 if request is not JSON', async () => {
+    await request(app)
+      .post('/openai/chat')
+      .send('are you going to take over the world?')
+      .expect(400);
+  });
+
+  test('should return 400 if request if there is no message property', async () => {
+    await request(app)
+      .post('/openai/chat')
+      .set('Content-Type', 'application/json')
+      .send({ messages: 'are you going to take over the world?' })
+      .expect(400);
+  });
 });
